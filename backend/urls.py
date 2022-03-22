21fea10821249b53195b1480b95e9141270eb8ab
anhnghia47 <trongnghiafes@gmail.com>
2022-03-23 00:07:11 +0700

    Update deploy 6

--- backend/urls.py
+++ backend/urls.py
@@ -1,9 +1,12 @@
+from re import template
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('ntnwine/', include('ntnwine.urls'))
+    path('ntnwine/', include('ntnwine.urls')),
+    re_path('.*', TemplateView.as_view(template_name='index.html'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
