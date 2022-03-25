bd59ee98fe235672212de4045b1d0e802c92b61e
anhnghia47 <trongnghiafes@gmail.com>
2022-03-26 00:44:31 +0700

    update url home

--- ntnwine/urls.py
+++ ntnwine/urls.py
@@ -5,7 +5,7 @@ from .auth import CustomAuthToken
 
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    # path('', views.home, name='home'),
     path('products', views.products, name='products'),
     path('products_a_page/<str:index_page>', views.products_a_page, name='products_a_page'),
     path('detail-with-code/<str:product_code>', views.product_detail_with_code, name='product_detail_with_code'),
