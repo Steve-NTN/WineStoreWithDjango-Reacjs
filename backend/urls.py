f3a89a783c32697fb3653f692f7247341c0d7117
anhnghia47 <trongnghiafes@gmail.com>
2022-03-28 21:45:53 +0700

    update deploy 21

--- backend/urls.py
+++ backend/urls.py
@@ -3,10 +3,10 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import index
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', index, name='index.html'),
+    path('', TemplateView.as_view(template_name='index.html'), name='index'),
     path('admin/', admin.site.urls),
     path('api/', include('ntnwine.urls'))
 ] + static(
